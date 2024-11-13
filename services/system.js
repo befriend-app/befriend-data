0b10aeb0e6e2dac6613792f2b061f98a5efd3cea
egekhter <egekhter@gmail.com>
2024-11-13 17:55:18 -0600

    system service

---
+++ services/system.js
@@ -0,0 +1,11 @@
+module.exports = {
+    keys: {
+        music: {
+            genres: 'music_genres_last_country',
+            artists: {
+                country: 'music_artists_last_country',
+                country_genre: 'music_artists_last_country_genre'
+            }
+        }
+    }
+}
